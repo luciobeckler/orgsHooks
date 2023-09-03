8ac1f11e21597fbef7099d1c3cdcd27fdd2655c4
Lucio_Beckler <luciobecklerp@gmail.com>
2023-09-03 20:54:00 -0300

    Criando o componente estrelas

--- src/components/Estrelas.js/index.js
+++ src/components/Estrelas.js/index.js
@@ -1,16 +1,30 @@
 import React from 'react'
-import { Image } from 'react-native'
+import { Image, StyleSheet, View } from 'react-native'
 
 export default function Estrelas({
     estrelaAtiva,
     estralaDesativada,
     quantidade: quantidadeAntiga,
     editavel = false,
-    grande = false,
+    grande
 }) {
+
+    const estilos = estilosFuncao(grande)
     return (
-        <>
-            <Image source={estrelaAtiva}></Image>
-        </>
+        <View style={estilos.container}>
+            <Image style={estilos.estrela} source={estrelaAtiva}></Image>
+            <Image style={estilos.estrela} source={estrelaAtiva}></Image>
+        </View>
     )
 }
+
+const estilosFuncao = (grande) => StyleSheet.create({
+    estrela: {
+        width: grande ? 36 : 12,
+        height: grande ? 36 : 12,
+        marginRight: 2
+    },
+    container: {
+        flexDirection: 'row'
+    }
+})
